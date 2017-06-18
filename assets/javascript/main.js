cbdeaf00cef3078aa72fafaabf228893f4716f50
Andy Lushman <andylushman@gmail.com>
2017-06-17 21:06:48 -0600

    Added jumbotron

---
+++ assets/javascript/main.js
@@ -0,0 +1,28 @@
+//=======================
+//GLOBAL VARIABLES
+//=======================
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDIXjfHrNm-LzFJZT-wUc5nMNxXhWVorq0",
+    authDomain: "train-scheduler-77d8d.firebaseapp.com",
+    databaseURL: "https://train-scheduler-77d8d.firebaseio.com",
+    projectId: "train-scheduler-77d8d",
+    storageBucket: "train-scheduler-77d8d.appspot.com",
+    messagingSenderId: "441578825451"
+  };
+
+var database = firebase.database();
+
+//=======================
+//FUNCTIONS
+//=======================
+
+
+
+//=======================
+//MAIN PROCESS
+//=======================
+
+//Initialize Friebase
+  firebase.initializeApp(config);
